a9ea0f24fef1521ae8302bf7ae98f3d0cbc6e17c
morb_au <morb@beagle.com.au>
2010-10-22 09:57:00 +1000

    Serialise the operation of 4a.cm-psql-copy-files-to-osm-api.bash; Let 4a.cm-psql-copy-files-to-osm-api.bash clean up input files when processed.

--- 4a.cm-psql-copy-files-to-osm-api.bash
+++ 4a.cm-psql-copy-files-to-osm-api.bash
@@ -7,6 +7,26 @@
 
 CHANGESET_UPLOAD_BASE=/tmp/commonmap/bulk-uploads
 CHANGESET_UPLOAD_PATH=$CHANGESET_UPLOAD_BASE/for-upload
+CHANGESET_SQL_PATH=$CHANGESET_UPLOAD_BASE/sql
+
+# Make sure only one copy runs at a time
+LOCKFILE=/tmp/4a.cm-psql-copy-files-to-osm-api.bash.lock
+ 
+if [ -f $LOCKFILE ]; then
+  if [ "$(ps -p `cat $LOCKFILE` | wc -l)" -gt 1 ]; then
+    # process is still running
+    echo "Locked"
+    exit 1
+  else
+    # process not running, but lock file not deleted?
+    echo "WARNING: Previous execution did not run to completion"
+    rm $LOCKFILE
+  fi
+fi
+echo $$ > $LOCKFILE 
+
+
+# Now for the script proper
 
 # Add PostgreSQL 9.0 path for access to pg_config
 # Should have no side-effect for PostgreSQL 8.x-only systems
@@ -15,10 +35,6 @@ PATH=/usr/pgsql-9.0/bin:$PATH
 
 cd $CHANGESET_UPLOAD_BASE
 
-# Clear out detritus from previous runs
-#  otherwise will confuse 'ls $CHANGESET_UPLOAD_PATH' below
-rm -rf $CHANGESET_UPLOAD_PATH
-rm *.sql
 
 #
 # Go through each file in the queue
@@ -26,12 +42,19 @@ rm *.sql
 for BZ2_UPLOAD in cm.*.bz2
 do
 
+# Clear out detritus from previous runs
+#  otherwise will confuse 'ls $CHANGESET_UPLOAD_PATH' below
+rm -rf $CHANGESET_UPLOAD_PATH
+rm upload.*.sql
+
 tar -jxvf $BZ2_UPLOAD
 
 for UPLOAD_CHANGESET in $( ls $CHANGESET_UPLOAD_PATH )
 do
 
-cat <<EOL > $CHANGESET_UPLOAD_BASE/upload.$UPLOAD_CHANGESET.sql
+mkdir -p $CHANGESET_SQL_PATH
+
+cat <<EOL > $CHANGESET_SQL_PATH/upload.$UPLOAD_CHANGESET.sql
  
 BEGIN;
 
@@ -62,12 +85,18 @@ COMMIT;
 EOL
 
 
-chown -R postgres:postgres $CHANGESET_UPLOAD_PATH
+chown -R postgres:postgres $CHANGESET_UPLOAD_BASE
 
-su - postgres -c "psql -d commonmap_pr -U commonmap_pr < $CHANGESET_UPLOAD_BASE/upload.$UPLOAD_CHANGESET.sql"  
+su - postgres -c "psql -d commonmap_pr -U commonmap_pr < $CHANGESET_SQL_PATH/upload.$UPLOAD_CHANGESET.sql"  
 
 done
+
+rm $BZ2_UPLOAD
+
 done
+
+
+rm $LOCKFILE 
 
 #
 # ENDS.
